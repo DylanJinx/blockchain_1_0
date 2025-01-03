0661ee878b95fdbe22a91ef5b5a3d0a9b2e3f4d8
DylanJinx <2636353989@qq.com>
2025-01-03 17:05:07 +0800

    test header

--- core/block_test.go
+++ core/block_test.go
@@ -1,9 +1,27 @@
 package core
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+	"time"
+
+	"github.com/DylanJinx/blockchain_1_0/types"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestHeader_Encode_Decode(t *testing.T) {
 	h := &Header{
-		
+		Version  : 1,
+		PrevBlock: types.RandomHash(),
+		Timestamp: time.Now().UnixNano(),
+		Height   : 10,
+		Nonce    : 989394,
 	}
+
+	buf := &bytes.Buffer{}
+	assert.Nil(t, h.EncodeBinary(buf))
+
+	hDecode := &Header{}
+	assert.Nil(t, hDecode.DecodeBinary(buf))
+	assert.Equal(t, h, hDecode)
 }
